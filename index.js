e3ce06b12ccd17144b5ee64fc749dc53468b9034
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2020-12-01 11:38:54 -0800

    Many minor fixes
    Folder names updated, level control buttons finished, champion stat tracking w/ level currently WIP

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('Express');
 const axios = require('axios');
 const app = express();
 const path = require('path');
-const { dirname } = require('path');
 
 
 
@@ -11,17 +10,38 @@ app.set('views', path.join(__dirname, '/views'));
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+const isClientReq = (params, clientCB) => {
+    if (params.src === "client") {
+        clientCB();
+    } 
+}
+
 app.get('/champions/:champion', async (req, res) => {
     let { champion } = req.params;
     champion = champion.charAt(0).toUpperCase() + champion.slice(1);
     try {
         const reqDDragon = await axios("http://ddragon.leagueoflegends.com/cdn/10.23.1/data/en_US/champion.json?api-key=RGAPI-52196e30-a662-4dfd-9ad8-e53dc023a0fc");
+
         const { name, title, blurb, tags, image, stats} = reqDDragon.data.data[champion];
 
         const champImg = image.full;
+
+        const mainStats = {
+            hp: [stats.hp, stats.hpperlevel],
+            mp: [stats.mp, stats.mpperlevel],
+            ms: [stats.movespeed, 0],
+            armor: [stats.armor, stats.armorperlevel],
+            mr: [stats.spellblock, stats.spellblockperlevel],
+            attRange: [stats.attackrange, 0],
+            hpreg: [stats.hpregen, stats.hpregenperlevel],
+            mpreg: [stats.mpregen, stats.mpregenperlevel],
+            crit: [stats.crit, stats.critperlevel],
+            ad: [stats.attackdamage, stats.attackdamageperlevel],
+            as: [stats.attackspeed, stats.attackspeedperlevel]
+        }
  
         console.log(stats);
-        res.render('champTest', { name, title, champImg, blurb, stats});
+        res.render('champTest', { name, title, champImg, blurb, mainStats, stats});
     } catch(e){
         console.log(`ERROR: Try messing with the code. MAKE SURE YOU'RE NOT OVER-REQUESTING! error: ${e}`);
     }
--- public/assets/champTest.js
+++ public/assets/champTest.js
@@ -1,9 +1,29 @@
+// Code for level handler buttons
 const levelUp = document.querySelector('#levelup');
 const levelDown = document.querySelector('#leveldown');
 const reset = document.querySelector('#levelreset');
+const visualLevel = document.querySelector('#level');
 
-let level = 0;
+let level = 1;
 
-levelUp.addEventListener('click', () => {level++; console.log(level)});
-levelDown.addEventListener('click', () => {level--; console.log(level)});
-reset.addEventListener('click', () => {level = 0; console.log(level)});
+levelUp.addEventListener('click', () => {
+    level++; 
+    visualLevel.innerText = level;
+    updateStats(level);
+});
+levelDown.addEventListener('click', () => {level--; visualLevel.innerText = level});
+reset.addEventListener('click', () => {level = 1; visualLevel.innerText = level});
+
+// Code for stat tracking w/ level
+
+// Go through every mainStat and modify them based on their level and respective modStat
+const updateStats = async (level) => {
+    const champName = document.querySelector(".name");
+    const cMainStats = await axios.get(`/champions/:${champName.innerText}`, { reqsrc: "client" })
+    console.log(cMainStats);
+    for (stat in cMainStats) {
+        stat[0] += stat[1] * level;
+        // document.querySelector(`#${stat}`).innerText = stat[0];
+        console.log(cMainStats);
+    }
+}
